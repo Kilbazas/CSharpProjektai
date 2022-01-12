b840e92ad3b94c0a732da72a7321621fa69be390
Kilbazas <35705748+Kilbazas@users.noreply.github.com>
2022-01-13 01:00:50 +0200

    Update PersonalNumberChecker.c

--- PersonalNumberChecker.c
+++ PersonalNumberChecker.c
@@ -10,23 +10,23 @@ namespace PersonalNumberChecker
             {
                 Console.WriteLine("Įvesk asmens kodą: ");
 
-                string asmensKodas = Console.ReadLine();
+                string personalNumber = Console.ReadLine();
 
-                if (long.TryParse(asmensKodas, out _) == false)
+                if (long.TryParse(personalNumber, out _) == false)
                 {
                     Console.WriteLine("Asmens kodas turi būti sudarytas tik iš skaičių");
                 }
-                else if (asmensKodas[0] != '3' && asmensKodas[0] != '4')
+                else if (personalNumber[0] != '3' && personalNumber[0] != '4')
                 {
                     Console.WriteLine("Asmens kodas turėtu prasidėti skaičiumi 3 arba 4");
                 }
-                else if (asmensKodas.Length < 11 || asmensKodas.Length > 11)
+                else if (personalNumber.Length < 11 || personalNumber.Length > 11)
                 {
                     Console.WriteLine("Įvedei per mažai arba per daug skaičių - bandyk dar kartą");
                 }
                 else
                 {
-                    Console.WriteLine("Tavo asmens kodas yra: " + asmensKodas);
+                    Console.WriteLine("Tavo asmens kodas yra: " + personalNumber);
                     break;
                 }
                     
