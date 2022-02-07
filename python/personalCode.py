5f44b8d59125c0d9fb6bafbd5365040595568e6c
Kilbazas <35705748+Kilbazas@users.noreply.github.com>
2022-02-08 00:06:33 +0200

    Update personalCode.py

--- python/personalCode.py
+++ python/personalCode.py
@@ -1,44 +1,34 @@
 
 def personal_code_lenght_checker(person_code_param):
-    result = False
     person_code_param = str(person_code_param)
-    if len(person_code_param) == 11:
-        result = True
-    return result
+    return len(person_code_param) == 11
 
 def is_entered_personal_code_is_number(person_code_param):
-    result = True
     person_code_param = str(person_code_param)
     for x in range(len(person_code_param)):
         if(ord(person_code_param[x]) < 48 or ord(person_code_param[x]) > 57): 
-            result = False
-            break
-    return result
+            return False
+            
+    return True
 
 def get_birth_date_from_personal_code(person_code_param):
     return person_code_param[0:7] 
 
 def is_last_number_is_exeption_for_no_birth_date(person_code_param):
-    result = False 
     birth_date = get_birth_date_from_personal_code(person_code_param)
     if birth_date[1:3] == "00" or birth_date[3:5] == "00":
-        result = True
-    return result
+        return True
+    return False
 
 def is_personal_code_exeptional(person_code_param):
-    result = False
-    input_first_number = person_code_param[0]
-    if input_first_number == '9':
-        result = True
-    return result
+    return person_code_param[0] == '9'
 
 def is_entered_personal_code_first_number_valid(person_code_param):
-    result = False
     first_number_list = ["1", "2", "3", "4", "5", "6", "9"]
     first_number = person_code_param[0]
     if first_number in first_number_list:
-        result = True
-    return result
+        return True
+    return False
 
 def what_year_is_it(person_code_param):
     year = 5
@@ -53,27 +43,24 @@ def what_year_is_it(person_code_param):
     return int(year)
 
 def is_entered_personal_code_is_leaf_year(person_code_param):
-    result = False
     year = what_year_is_it(person_code_param)
     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-        result = True
-    return result
+        return True
+    return False
 
 def is_entered_personal_code_month_is_in_range(person_code_param):
-    result = False  
     birth_date = get_birth_date_from_personal_code(person_code_param)
     month = birth_date[3:5]
     month = int(month)
     if 0 < month < 13:
-        result = True 
+        return True 
     if is_personal_code_exeptional(person_code_param) == True:
-        result = True
+        return True
     if is_last_number_is_exeption_for_no_birth_date(person_code_param) == True:
-        result = True 
-    return result
+        return True 
+    return False
 
 def is_entered_personal_code_day_is_in_range(person_code_param):
-    result = False  
     birth_date = get_birth_date_from_personal_code(person_code_param)
     days_in_month_list_leaf = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     days_in_month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -83,15 +70,14 @@ def is_entered_personal_code_day_is_in_range(person_code_param):
     month = int(month)
     if is_entered_personal_code_is_leaf_year(person_code_param) == True:
         if days <= days_in_month_list_leaf[month - 1]:
-            result = True
+            return True
     if days <= days_in_month_list[month - 1]:
-        result = True 
+        return True 
     if is_personal_code_exeptional(person_code_param) == True:
-        result = True
-    return result
+        return True
+    return False
 
 def is_last_number_of_personal_code_is_legit(person_code_param):
-    result = False
     input_last_number = person_code_param
     last_number = ((int(input_last_number[0]) * 1 + int(input_last_number[1]) * 2 + int(input_last_number[2]) * 3 + int(input_last_number[3]) * 4 + int(input_last_number[4]) * 5 +
                   int(input_last_number[5]) * 6 + int(input_last_number[6]) * 7 + int(input_last_number[7]) * 8 + int(input_last_number[8]) * 9 + int(input_last_number[9]) * 1)  % 11)
@@ -101,24 +87,22 @@ def is_last_number_of_personal_code_is_legit(person_code_param):
         if last_number == 10:
             last_number = 0 
     if int(input_last_number[10]) == last_number:
-        result = True
+        return True 
     if is_personal_code_exeptional(person_code_param) == True:
-        result = True
-    return result 
+        return True
+    return False 
     
 def is_person_isnt_too_old(person_code_param):
-    result = False
     year = what_year_is_it(person_code_param)
     if year > 1890:
-        result = True
-    return result
+        return True 
+    return False
 
 def is_person_isnt_too_young(person_code_param):
-    result = False
     year = what_year_is_it(person_code_param)
     if year < 2023:
-        result = True
-    return result
+        return True
+    return False
 
 def verify_personal_code(personal_code):
     if personal_code == None:
@@ -130,6 +114,7 @@ def verify_personal_code(personal_code):
     input = "".join(personal_code.split())
     if personal_code != input:
         return "Personal code should be written without spaces"
+
     if personal_code_lenght_checker(input) == False:
         return "Personal code is too short or too long."
     if is_entered_personal_code_is_number(input) == False:
@@ -153,58 +138,40 @@ def verify_personal_code(personal_code):
 
 if  verify_personal_code(None) == "valid":
     print("Personal code input can not be None.")
-
 if  verify_personal_code("") == "valid":
     print("Personal code input can not be empty.")
-
 if  verify_personal_code("3 9 6 0124 00 40") == "valid": #Logika is not found. Ar reiktu tikrint FALSE?
     print("Method join does not work.")
-
 if  verify_personal_code("396012400400") == "valid":
     print("Personal code lenght checker is not working.")
-
 if  verify_personal_code("3960124004") == "valid":
     print("Personal code lenght checker is not working.")
-
 if  verify_personal_code("LT396012400") == "valid":
     print("Personal code symbol checker does not work.")
-
 if  verify_personal_code("79601240040") == "valid":
     print("Personal code first symbol checker does not work.")
-
 if  verify_personal_code("39615240040") == "valid":
     print("Personal code month checker does not work.")
-
 if  verify_personal_code("39600240046") == False:
     print("Personal code month checker does not work. If someone doesn't know birth month case.")
-
 if  verify_personal_code("99600240040") == False:
     print("Personal code month checker does not work. If code starts with 9 case.")
-
 if  verify_personal_code("39601000044") == False:
     print("Personal code day checker does not work. If someone does't know birth day case.")
-
 if  verify_personal_code("39601400040") == "valid":
     print("Personal code day checker does not work. Leaf year case.")
-
 if  verify_personal_code("39602290047") == False:
     print("Personal code day checker does not work. Leaf year case.")
-
 if  verify_personal_code("99601400040") == False:
     print("Personal code day checker does not work. If code starts with 9 case.")
-
 if  verify_personal_code("39601240043") == "valid":
     print(f"Personal code last number checker does't work. ")
-
 if  verify_personal_code("17901240040") == "valid":
     print("Person is too old")
-
 if  verify_personal_code("27901240040") == "valid":
     print("Person is too old")
-
 if  verify_personal_code("59601240040") == "valid":
     print("Person is too young")
-
 if  verify_personal_code("69601240040") == "valid":
     print("Person is too young")
 
